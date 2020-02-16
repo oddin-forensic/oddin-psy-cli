3e7650bd4656b646e9076ed091fb21926714fb29
Junior santos <juniorsnts123@gmail.com>
2020-02-15 21:56:38 -0300

    Gerando csv baseado no retorno do sql

--- src/commands/odin-psy.js
+++ src/commands/odin-psy.js
@@ -34,35 +34,49 @@ const command = {
       console.log('connected to DB');
     });
 
-    function createCsv(headers, recordes) {
+    function createCsv(recordes) {
       const csvWriter = createCsvWriter({
         path: output_dir + '/teste.csv',
         header: [
-          { id: 'name', title: 'NAME' },
-          { id: 'lang', title: 'LANGUAGE' }
+          { id: 'file_obj_id', title: 'file_obj_id' },
+          { id: 'file_ctime', title: 'file_ctime' },
+          { id: 'file_mtime', title: 'file_mtime' },
+          { id: 'file_crtime', title: 'file_crtime' },
+          { id: 'file_atime', title: 'file_atime' },
+          { id: 'file_name', title: 'file_name' },
+          { id: 'file_md5', title: 'file_md5' },
+          { id: 'attribute_type_name', title: 'attribute_type_name' },
+          { id: 'attribute_value', title: 'attribute_value' },
+          { id: 'artifact_type_name', title: 'artifact_type_name' },
+          { id: 'artifact_type_display_name', title: 'artifact_type_display_name' },
+          { id: 'layout_byte_start', title: 'layout_byte_start' },
+          { id: 'layout_byte_len', title: 'layout_byte_len' },
+          { id: 'layout_sequence', title: 'layout_sequence' },
         ]
       });
 
       const records = [
-        { name: 'Bob', lang: 'French, English' },
-        { name: 'Mary', lang: 'English' }
+        ...recordes
       ];
 
       csvWriter.writeRecords(records)       // returns a promise
         .then(() => {
-          console.log('...Done');
+          console.log('Sucess');
         });
     }
 
+    let sql = 'select tsk_files.obj_id as file_obj_id, tsk_files.ctime as file_ctime, tsk_files.mtime as file_mtime, tsk_files.crtime as file_crtime, tsk_files.atime as file_atime, tsk_files.name as file_name, tsk_files.md5 as file_md5, blackboard_attribute_types.display_name as attribute_type_name, blackboard_attribute_types.type_name as attribute_type_name, blackboard_attributes.value_text as attribute_value, blackboard_artifact_types.type_name as artifact_type_name, blackboard_artifact_types.display_name as artifact_type_display_name, tsk_file_layout.byte_start as layout_byte_start, tsk_file_layout.byte_len as layout_byte_len, tsk_file_layout.sequence as layout_sequence from tsk_files inner join blackboard_artifacts on blackboard_artifacts.obj_id = tsk_files.obj_id inner join blackboard_artifact_types on blackboard_artifact_types.artifact_type_id = blackboard_artifacts.artifact_type_id inner join blackboard_attributes on blackboard_attributes.artifact_id = blackboard_artifacts.artifact_id inner join blackboard_attribute_types on blackboard_attribute_types.attribute_type_id = blackboard_attributes.attribute_type_id inner join tsk_file_layout on tsk_file_layout.obj_id = tsk_files.obj_id order by tsk_files.obj_id';
+
     db_sqlite.serialize(() => {
-      db_sqlite.all('SELECT * FROM image_gallery_db_info', (err, rows) => {
+      db_sqlite.all(sql, (err, rows) => {
         if (err) {
           console.log(err)
         }
-        createCsv('teste', 'reste');
+        // console.log(rows);
+        createCsv(rows);
       })
     });
   }
 }
 
-module.exports = command
+module.exports = command;
